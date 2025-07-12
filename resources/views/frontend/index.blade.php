415d64b9cc17b1fc588841c4f494fd8b251d6d34
rickyfauzi <rickyfauzi116@gmail.com>
2025-07-12 21:55:27 +0700

    lagi test

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,7 +9,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Document</title>
+{{-- <title>Document</title> --}}
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('assets2/css/animate.css') }}" />
@@ -30,7 +30,7 @@
                     <div class="col-lg-12">
                         <div class="ud-hero-content wow fadeInUp" data-wow-delay=".2s">
                             <h1 class="ud-hero-title">
-                                Selamat Datang di Website GenBI Cirebon cuy, Portal Berita dan Sistem Absensi Terintegrasi
+                                Selamat Datang di Website GenBI Cirebon cuyyy, Portal Berita dan Sistem Absensi Terintegrasi
                             </h1>
                             <p class="ud-hero-desc">
                                 <span id="typed-text-1">GenBI, Energi untuk Negeri </span><br>
@@ -412,7 +412,7 @@
         </section>
 
 
-     
+
         <a href="javascript:void(0)" class="back-to-top">
             <i class="lni lni-chevron-up"></i> <!-- Ikon back-to-top -->
         </a>
@@ -603,7 +603,7 @@
             items.forEach(item => item.addEventListener('click', toggleAccordion));
         </script>
 
-       
+
 
     </body>
 
