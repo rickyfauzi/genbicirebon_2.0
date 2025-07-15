5487e4fd5bec95a0b961abe979dce7f2f4be28d7
rickyfauzi <rickyfauzi116@gmail.com>
2025-07-15 16:35:27 +0700

    dede

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -6,30 +6,31 @@ use Illuminate\Http\Request;
 use Google\Cloud\Dialogflow\V2\SessionsClient;
 use Google\Cloud\Dialogflow\V2\QueryInput;
 use Google\Cloud\Dialogflow\V2\TextInput;
+use GPBMetadata\Google\Api\Http;
 
 class ChatbotController extends Controller
 {
-    public function detectIntent(Request $request)
+    public function handle(Request $request)
     {
-        $text = $request->input('message');
-        $projectId = 'learned-fusion-460215';
+        $message = $request->input('message');
 
-        $sessionClient = new SessionsClient();
-        $session = $sessionClient->sessionName($projectId, uniqid());
+        // Dialogflow Webhook or Detect Intent API
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . env('DIALOGFLOW_TOKEN'),
+        ])->post('https://dialogflow.googleapis.com/v2/projects/genbichatbot-gjxn/agent/sessions/123456:detectIntent', [
+            'query_input' => [
+                'text' => [
+                    'text' => $message,
+                    'language_code' => 'id',
+                ]
+            ]
+        ]);
 
-        $textInput = new TextInput();
-        $textInput->setText($text);
-        $textInput->setLanguageCode('id'); // bahasa Indonesia
-
-        $queryInput = new QueryInput();
-        $queryInput->setText($textInput);
-
-        $response = $sessionClient->detectIntent($session, $queryInput);
-        $result = $response->getQueryResult();
-        $reply = $result->getFulfillmentText();
-
-        $sessionClient->close();
-
-        return response()->json(['reply' => $reply]);
+        if ($response->successful()) {
+            $reply = $response->json()['queryResult']['fulfillmentText'] ?? 'Tidak ada jawaban.';
+            return response()->json(['reply' => $reply]);
+        } else {
+            return response()->json(['reply' => 'Maaf, terjadi kesalahan.']);
+        }
     }
 }
